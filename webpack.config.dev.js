a8fefe627dc1d48c10cdf668b619498a8637c5db
jbont <j@b.c>
2017-01-08 17:21:35 +0100

    Favicon

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -30,7 +30,12 @@ export default {
       {test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file'},
       {test: /\.(woff|woff2)$/, loader: 'url?prefix=font/&limit=5000'},
       {test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/octet-stream'},
-      {test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=image/svg+xml'}
+      {test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=image/svg+xml'},
+      // {
+      //   test: /\.(jpg|jpeg|gif|png|ico)$/,
+      //   exclude: /node_modules/,
+      //   loader:'file-loader?name=img/[path][name].[ext]&context=./src'
+      // }
     ]
   }
 };
