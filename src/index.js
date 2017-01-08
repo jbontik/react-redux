830825245d2498992330b39aeaa853445d969b5d
jbont <j@b.c>
2017-01-08 17:39:48 +0100

    Add store to entry point index.js (wrap router within provider (from react-redux))

--- src/index.js
+++ src/index.js
@@ -5,7 +5,13 @@ import routes from "./routes";
 import './styles/styles.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'babel-polyfill';
+import {Provider} from 'react-redux';
+import configureStore from './store/configureStore';
+
+const store = configureStore();
 
 ReactDOM.render(
+  <Provider store={store}>
     <Router history={browserHistory} routes={routes}/>, document.getElementById('app')
-  );
+  </Provider>
+);
