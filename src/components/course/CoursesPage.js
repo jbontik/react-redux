776f3b34ef24ed7debb03e0641050d5ebde0acc3
jbont <j@b.c>
2017-01-08 17:54:43 +0100

    Formatting

--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {connect,} from 'react-redux';
-import *as courseActions from '../../actions/courseActions';
+import * as courseActions from '../../actions/courseActions';
 
 class CoursesPage extends React.Component {
 
