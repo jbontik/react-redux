bc42a5421efbbc598aae85336751f2ad817728b6
jbont <j@b.c>
2017-01-08 09:26:09 +0100

    Add course pages component, and a route to it

--- src/routes.js
+++ src/routes.js
@@ -2,10 +2,12 @@ import React from 'react';
 import App from "./components/App";
 import HomePage from "./components/home/HomePage";
 import AboutPage from "./components/about/AboutPage";
+import CoursesPage from "./components/course/CoursesPage";
 import {Route, IndexRoute} from 'react-router';
 export default (
   <Route path="/" component={App}>
     <IndexRoute component={HomePage}/>
+    <Route path="courses" component={CoursesPage}/>
     <Route path="about" component={AboutPage}/>
   </Route>
 );
